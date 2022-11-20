d892625c78bc522f6437588cf5ae75c270947847
wulonmt <zuck0087285@gmail.com>
2022-11-20 16:28:39 +0800

    fix init param

--- flw_airsim/airsim_server.py
+++ flw_airsim/airsim_server.py
@@ -2,16 +2,28 @@ from typing import List, Tuple
 
 import flwr as fl
 from flwr.common import Metrics
+from stable_baselines3 import SAC
+from airsim_client import AirsimClient, CustomCombinedExtractor
+from flwr.common.typing import Parameters
+from flwr.common.parameter import ndarrays_to_parameters
 
-# Define strategy
-strategy = fl.server.strategy.FedAdam(min_fit_clients=2,
-                                    min_evaluate_clients=2,
-                                    min_available_clients=2,
-                                    )
 
-# Start Flower server
-fl.server.start_server(
-    server_address="192.168.1.85:8080",
-    config=fl.server.ServerConfig(num_rounds=5),
-    strategy=strategy,
-)
+def main():
+    init_model = AirsimClient()
+    initial_parameters = init_model.get_parameters(None)
+    initial_parameters = ndarrays_to_parameters(initial_parameters)
+    # Define strategy
+    strategy = fl.server.strategy.FedAdam(initial_parameters = initial_parameters,
+                                        min_fit_clients=2,
+                                        min_evaluate_clients=2,
+                                        min_available_clients=2,
+                                        )
+    # Start Flower server
+    fl.server.start_server(
+        server_address="192.168.1.85:8080",
+        config=fl.server.ServerConfig(num_rounds=5),
+        strategy=strategy,
+    )
+    
+if __name__ == "__main__":
+    main()
