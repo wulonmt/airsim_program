124bdbe5dc2fbe1ae4b10c44d246c295f7cedbfb
wulonmt <zuck0087285@gmail.com>
2022-12-22 03:30:56 +0800

    modified for episode checkpoint

--- car_env_test/SAC_car.py
+++ car_env_test/SAC_car.py
@@ -9,10 +9,11 @@ from stable_baselines3 import SAC
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, CallbackList, StopTrainingOnMaxEpisodes
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
-
 from stable_baselines3.common.vec_env import VecFrameStack
+
+from EpisodeCheckpointCallback import EpisodeCheckpointCallback
 
 import argparse
 
@@ -46,7 +47,6 @@ model = SAC( #action should be continue
     "CnnPolicy",
     env,
     learning_rate=0.0003,
-    policy_kwargs=policy_kwargs,
     verbose=1,
     batch_size=64,
     train_freq=1,
@@ -57,7 +57,7 @@ model = SAC( #action should be continue
 )
 
 # Create an evaluation callback with the same env, called every 10000 iterations
-callbacks = []
+callback_list = []
 eval_callback = EvalCallback(
     env,
     callback_on_new_best=None,
@@ -67,10 +67,24 @@ eval_callback = EvalCallback(
     eval_freq=10000,
     verbose = 1
 )
-callbacks.append(eval_callback)
+callback_list.append(eval_callback)
 
-kwargs = {}
-kwargs["callback"] = callbacks
+# Save a checkpoint every 1000 steps
+ep_checkpoint_callback = EpisodeCheckpointCallback(
+  check_episodes=5,
+  save_path="./checkpoint/",
+  name_prefix="rl_model",
+  save_replay_buffer=True,
+  save_vecnormalize=True,
+  verbose=2
+)
+callback_list.append(ep_checkpoint_callback)
+
+# Stops training when the model reaches the maximum number of episodes
+callback_max_episodes = StopTrainingOnMaxEpisodes(max_episodes=1e4, verbose=1)
+callback_list.append(callback_max_episodes)
+
+callback = CallbackList(callback_list)
 
 #make time eazier to read
 Ttime = str(time.ctime())
@@ -87,7 +101,7 @@ print("Start time: ", t)
 
 # Train for a certain number of timesteps
 model.learn(
-    total_timesteps=5e6, tb_log_name="SAC_airsim_car_run_" + str(time.time()), **kwargs
+    total_timesteps=1e7, tb_log_name="SAC_airsim_car_run_" + str(time.time()), callback = callback
 )
 
 # Save policy weights
