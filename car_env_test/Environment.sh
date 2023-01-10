9766ddccbee796f7a456c1f39b412db40d7c9440
wulonmt <zuck0087285@gmail.com>
2023-01-10 16:42:09 +0800

    disable render

--- car_env_test/Environment.sh
+++ car_env_test/Environment.sh
@@ -1,7 +1,7 @@
 echo "Stating Environment"
 
 ~/AirSim/Unreal/Environments/AirSimNH/LinuxNoEditor/AirSimNH.sh \
--windowed -ResX=1024 -ResY=576 \
+-windowed -ResX=1024 -ResY=576 -NoVSync \
 -settings="/home/wulonmt/Desktop/project/program/airsim_program/car_env_test/settings.json"
 
 
