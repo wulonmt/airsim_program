e15dd689619013b124917a6f1c74118842b53b69
wulonmt <zuck0087285@gmail.com>
2023-01-10 16:49:00 +0800

    commit for ground1

--- car_env_test/Environment.sh
+++ car_env_test/Environment.sh
@@ -1,8 +1,8 @@
 echo "Stating Environment"
 
 ~/AirSim/Unreal/Environments/AirSimNH/LinuxNoEditor/AirSimNH.sh \
--windowed -ResX=1024 -ResY=576 \
--settings="/home/wulonmt/Desktop/project/program/airsim_program/car_env_test/settings.json"
+-windowed -ResX=1024 -ResY=576 -NoVSync \
+-settings="/home/user/wulonmt/airsim_program/car_env_test/settings.json"
 
 
 exit 0
